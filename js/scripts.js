e494f2da5d3f30bf81e81820ff9513cabeab9fda
iyerikuzwe <iyerikuzweregine19@gmail.com>
2019-05-08 18:55:49 +0200

    Imake change

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $(".clickable1").click(function() {
+      $("#three").toggle();
+      $("#quiz").toggle();
+    });
+    $(".clickable2").click(function() {
+        $("#four").toggle();
+        $("#search").toggle();
+      });
+    $(".clickable3").click(function() {
+      $("#five").toggle();
+      $("#quote").toggle();
+      }); 
+
+    $(".clickable4").click(function() {
+      $("#six").toggle();
+      $("#news").toggle();
+        }); 
+    $(".clickable5").click(function() {
+        $("#seven").toggle();
+        $("#gallery").toggle();
+            }); 
+    $(".clickable6").click(function() {
+        $("#eight").toggle();
+        $("#blog").toggle();
+                });    
+  });
+
